06cb9bcff64860640ee9613afc1f2ed491420d31
rai64521 <raisumant99@gmail.com>
2025-07-26 21:50:00 +0530

    git data added

--- chai/index.js
+++ chai/index.js
@@ -5,6 +5,42 @@ import 'dotenv/config'
 const app = express()
 const port = 4000
 
+const gitHubData = {
+  "login": "rai64521",
+  "id": 175352032,
+  "node_id": "U_kgDOCnOo4A",
+  "avatar_url": "https://avatars.githubusercontent.com/u/175352032?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/rai64521",
+  "html_url": "https://github.com/rai64521",
+  "followers_url": "https://api.github.com/users/rai64521/followers",
+  "following_url": "https://api.github.com/users/rai64521/following{/other_user}",
+  "gists_url": "https://api.github.com/users/rai64521/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/rai64521/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/rai64521/subscriptions",
+  "organizations_url": "https://api.github.com/users/rai64521/orgs",
+  "repos_url": "https://api.github.com/users/rai64521/repos",
+  "events_url": "https://api.github.com/users/rai64521/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/rai64521/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Sumant Rai",
+  "company": null,
+  "blog": "",
+  "location": "Mumbai",
+  "email": null,
+  "hireable": null,
+  "bio": "indian\r\n",
+  "twitter_username": null,
+  "public_repos": 20,
+  "public_gists": 0,
+  "followers": 5,
+  "following": 0,
+  "created_at": "2024-07-11T18:58:18Z",
+  "updated_at": "2025-07-07T05:18:23Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
@@ -19,6 +55,10 @@ app.get("/login",(req,res)=>{
 
 app.get("/youtube",(req,res)=>{
     res.send("<h2>Youtube: Sumant Rai</h2>")
+})
+
+app.get("/github",(req,res)=>{
+    res.json(gitHubData)
 })
 
 app.listen(process.env.PORT ,()=>{
